9bb65ba9e758362d87a09411a5e91999d26392e3
vjcrombie <victor.jones@crombielockwood.co.nz>
2017-07-02 12:41:28 +1200

    Small changes for mockup

--- src/app/shared/navcoin.service.ts
+++ src/app/shared/navcoin.service.ts
@@ -1,3 +1,15 @@
-/**
- * Created by victo on 6/1/2017.
- */
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+
+const URL = 'http://localhost:3000/api/';
+
+@Injectable()
+export class NavcoinService {
+  // constructor (private http: Http) {}
+  // loadItems() {
+  //   return this.http.get(URL)
+  //     .map(res => res.json())
+  //     .toPromise();
+  // }
+}
